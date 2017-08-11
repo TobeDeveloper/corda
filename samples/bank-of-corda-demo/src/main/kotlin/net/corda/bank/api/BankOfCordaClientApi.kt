c3fd0145153c67cc7cbf1880aa8278b4b27d2901
Ross Nicoll <ross.nicoll@r3.com>
2017-08-11 14:03:23 +0100

    Remove IssuerFlow
    * Remove IssuerFlow as it is dangerous and its presence in the finance module risks accidental use in non-test code. As written it will issue arbitary amounts of currency on request from any node on the network, with no validation barring that the currency type is valid.
    * Unify interface to CashIssueFlow to match the previous IssuerFlow

--- samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/api/BankOfCordaClientApi.kt
+++ samples/bank-of-corda-demo/src/main/kotlin/net/corda/bank/api/BankOfCordaClientApi.kt
@@ -5,12 +5,11 @@ import net.corda.client.rpc.CordaRPCClient
 import net.corda.core.contracts.Amount
 import net.corda.core.contracts.currency
 import net.corda.core.messaging.startFlow
-import net.corda.core.utilities.OpaqueBytes
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.NetworkHostAndPort
+import net.corda.core.utilities.OpaqueBytes
 import net.corda.core.utilities.getOrThrow
-import net.corda.flows.IssuerFlow.IssuanceRequester
-import net.corda.testing.DUMMY_NOTARY
+import net.corda.flows.CashIssueFlow
 import net.corda.testing.http.HttpApi
 
 /**
@@ -47,9 +46,9 @@ class BankOfCordaClientApi(val hostAndPort: NetworkHostAndPort) {
                     ?: throw IllegalStateException("Unable to locate notary node in network map cache")
 
             val amount = Amount(params.amount, currency(params.currency))
-            val issuerToPartyRef = OpaqueBytes.of(params.issueToPartyRefAsString.toByte())
+            val issuerBankPartyRef = OpaqueBytes.of(params.issuerBankPartyRef.toByte())
 
-            return rpc.startFlow(::IssuanceRequester, amount, issueToParty, issuerToPartyRef, issuerBankParty, notaryNode.notaryIdentity, params.anonymous)
+            return rpc.startFlow(::CashIssueFlow, amount, issueToParty, issuerBankParty, issuerBankPartyRef, notaryNode.notaryIdentity, params.anonymous)
                     .returnValue.getOrThrow().stx
         }
     }
